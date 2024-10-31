b7c4a29ed53fb9b2ff7e76c9dc6a21b80b338d0f
Dubby-Bantin <dubbybantin@gmail.com>
2024-11-01 00:21:54 +0100

    updated services page

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,8 +1,26 @@
 import bgImage from "../../assets/images/image-1.png";
-import photo from "../../assets/images/Rectangle 187.png";
+// import photo from "../../assets/images/Rectangle 187.png";
 import Footer from "../home/components/Footer";
 import photo3 from "../../assets/images/Rectangle 186.png";
-
+import { MdOutlinePhonelinkSetup, MdOutlineShowChart } from "react-icons/md";
+import photo4 from "../../assets/images/33801-2048x2048.jpg";
+import photo5 from "../../assets/images/2650106-2048x1269.jpg";
+import photo6 from "../../assets/images/38451-2048x2048.jpg";
+import photo7 from "../../assets/images/kisspng-closed-circuit-television-ip-camera-wireless-secur-centaur-3d-5b4b59d2d5ede6.9710647715316648508763.png";
+import photo8 from "../../assets/images/3315-2048x1241.jpg";
+import photo9 from "../../assets/images/19265-2048x1484.jpg";
+import photo10 from "../../assets/images/18941-2048x1541.jpg";
+import photo11 from "../../assets/images/Capture6-e1600421594258.jpg";
+import photo12 from "../../assets/images/5514-2048x1280.jpg";
+import { BsBarChartLine } from "react-icons/bs";
+import { FaChalkboardUser, FaFolderOpen, FaLock } from "react-icons/fa6";
+import { GoPerson } from "react-icons/go";
+import { GiBookmark } from "react-icons/gi";
+import { FaPeopleCarry } from "react-icons/fa";
+import { IoBugSharp } from "react-icons/io5";
+import { PiDeviceRotateThin, PiFileMagnifyingGlassThin } from "react-icons/pi";
+import { CiBasketball } from "react-icons/ci";
+import { LiaKeySolid, LiaToolsSolid } from "react-icons/lia";
 const Services = () => {
   return (
     <section>
@@ -30,43 +48,286 @@ const Services = () => {
           business and start-up computing needs and Data Center Infrastructures
         </p>
       </div>
-
-      <div className="p-5">
-        <h1 className="font-semibold text-3xl lg:text-4xl lg:leading-[4rem] mb-6 text-center lg:text-left sm:ml-16">
-          TECH INDUSTRY OUTLOOK
-        </h1>
+      <div className="flex sm:flex-row flex-col items-center justify-center">
+        <div className="p-5 sm:ml-16 mb-20">
+          <h1 className="font-semibold text-3xl lg:text-4xl lg:leading-[4rem] mb-6 text-center lg:text-left text-primaryBlue">
+            Enterprise Infrastructure
+          </h1>
+          <div className="w-28 border border-primaryBlue mb-2"></div>
+          <p className="text-sm mb-5">
+            To create the infrastructure that will deliver content to the right
+            place at the right time and budget
+          </p>
+          <ul className="list-disc text-gray-700 text-sm grid gap-2 mb-10">
+            <li>
+              Systems integrated and security to protect the integrity and
+              privacy of shared content across network
+            </li>
+            <li>
+              High-end and mid range enterprise server installation and
+              configuration
+            </li>
+            <li>Small business and start-up computing need</li>
+          </ul>
+          <div className="flex items-center">
+            <div className="flex flex-col items-center w-96 text-center">
+              <MdOutlineShowChart className="text-2xl mb-3" />
+              <p className="text-primaryBlue text-xl mb-3">
+                Network Infrastructure
+              </p>
+              <p className="text-gray-700 text-sm">
+                CPMS builds networks that meet clients’ demand for the structure
+                of extra components which are not strictly necessary to
+                functions in case of failure in other components.
+              </p>
+            </div>
+            <div className="flex flex-col items-center w-96 text-center">
+              <MdOutlinePhonelinkSetup className="text-2xl mb-3" />
+              <p className="text-primaryBlue text-xl mb-3">
+                Wireless and Mobile Connectivity
+              </p>
+              <p className="text-gray-700 text-sm">
+                Business is no longer stationary, and therefore enabling
+                wireless access to business resources on/off site has become
+                extremely important part of business requirement.
+              </p>
+            </div>
+          </div>
+        </div>
+        <img src={photo4} alt="" className="h-[30rem]" />
       </div>
 
-      <div className="flex flex-col lg:flex-row lg:justify-around lg:flex-wrap-reverse gap-6 px-6">
-        <img
-          src={photo}
-          alt="Image illustrating tech industry"
-          className="lg:w-1/3 w-full object-cover rounded-md h-[30rem]"
-        />
-        <div className="lg:w-1/2 flex flex-col gap-5 px-5 text-justify text-xl">
-          <p>
-            • Africa is one of the emerging tech landscapes in the digital
-            transforming era.
-          </p>
-          <p>
-            • The region highlights the existence of roughly 200 African
-            innovation hubs, 3,500 new tech-related ventures, and US $1 billion
-            in venture capital (VC) to a pan-African movement of start-up
-            entrepreneurs.
-          </p>
-          <p>
-            • Revenue in Africa’s eCommerce market is expected to show an annual
-            growth rate (2021-2025) of 13.27% resulting in a market volume of US
-            $40,758 million by 2025. Africa has the fastest-growing rate of
-            mobile penetration, with 44% of the population owning a device in
-            2018, and unique mobile subscribers expected to reach 634 million by
-            2025.
-          </p>
-          <p>
-            • Almost every booming industry in technology is data-driven. Hence,
-            there is an immense demand for technology-skilled professionals
-            across the region.
-          </p>
+      <div className="bg-primaryBlue/10 p-5 py-20 mb-10">
+        <div className="flex items-center justify-center gap-40">
+          <div>
+            <img src={photo5} alt="" className="h-[15rem]" />
+          </div>
+          <div className="">
+            <h1 className="font-semibold text-3xl lg:text-4xl lg:leading-[4rem] mb-6 text-center lg:text-left text-primaryBlue">
+              Enterprise Infrastructure
+            </h1>
+            <div className="w-28 border border-primaryBlue mb-2"></div>
+            <p className="text-sm mb-5">
+              We have competency in the deployment and management of the
+              following solutions
+            </p>
+            <ul className="list-disc text-gray-700 text-sm grid gap-2 mb-10">
+              <li className="">Corporate PBX</li>
+              <li className="">
+                Email Collaboration (Microsoft Exchange Mail and Open Source
+                Mail e.g. Scalix)
+              </li>
+              <li className="">VOIP (Voice Over IP Telephony)</li>
+              <li className="">Portal Servers and Services</li>
+              <li className="">Video Conferencing System</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-5 mb-10 flex items-start justify-center gap-40 ">
+        <div className="w-[70rem] flex items-center">
+          <div className="relative -top-16">
+            <h1 className="font-semibold text-3xl lg:text-4xl lg:leading-[4rem] mb-6 text-center lg:text-left text-primaryBlue">
+              Data Center Solution
+            </h1>
+            <div className="w-28 border border-primaryBlue mb-2"></div>
+            <p className="text-gray-700 text-sm">
+              Data Center Infrastructures are physical or virtual infrastructure
+              used by enterprises to house computer, server and networking
+              systems and other vectors acting in different directions which are
+              together equivalent for company’s information technology (IT)
+              needs, which typically involve storing, processing and serving
+              large amount of mission-critical data to clients in a
+              client/server construction
+            </p>
+          </div>
+          <img src={photo6} alt="" className="h-[30rem]" />
+        </div>
+      </div>
+      <div className="p-5 py-20 mb-10 flex justify-center gap-40 bg-primaryBlue/10">
+        <div className="w-[70rem] flex items-start justify-center gap-20">
+          <img src={photo7} alt="" className="h-[15rem]" />
+          <div className="relative -top-25">
+            <h1 className="font-semibold text-3xl lg:text-4xl lg:leading-[4rem] mb-6 text-center lg:text-left text-primaryBlue">
+              IP Surveillance
+            </h1>
+            <div className="w-28 border border-primaryBlue mb-2"></div>
+            <div className="text-gray-700 text-sm grid gap-5">
+              <p>
+                The digital network video surveillance system provides a host of
+                benefits and advanced purposes that cannot be provided by an
+                analogue video surveillance system.
+              </p>
+              <p>
+                Center Point Multi Solutions provides a complete solution of IP
+                cameras, easy to access, secure storage and is a strong and
+                healthy network switching. We shall provide switches that offer
+                what is needed for a robust network.
+              </p>
+              <p>
+                In addition, our IP Surveillance solution is fully resilient to
+                and secured from hacking.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-5 mb-10 flex items-start justify-center gap-40 ">
+        <div className="w-[70rem] flex items-center">
+          <div className="relative">
+            <h1 className="font-semibold text-3xl lg:text-4xl lg:leading-[4rem] mb-6 text-center lg:text-left text-primaryBlue">
+              Cloud Computing
+            </h1>
+            <div className="w-28 border border-primaryBlue mb-2"></div>
+            <p className="text-gray-700 text-sm">
+              Cloud Computing is a general term for any activity that involves
+              delivering hosted services over the internet. These services are
+              roadly divided into three categories: Infrastructure-as-a-service
+              (IaaS), Platform-as-a-Service (PaaS) and Software-as-a-Service
+              (SaaS).
+            </p>
+            <p className="text-gray-700 text-sm">
+              Cloud computing enables companies to consume computer resources as
+              a utility just like electricity rather than having to build and
+              maintain computing infrastructures in-house. It provides
+              sufficient cost effective IT resources as cost on demand IT based
+              on the actual usage of the customer.
+            </p>
+          </div>
+          <img src={photo8} alt="" className="h-[20rem]" />
+        </div>
+      </div>
+      <div className="p-5 mb-40 flex items-start justify-center gap-40">
+        <div className="w-[75rem] flex items-center gap-10">
+          <div className="relative">
+            <h1 className="font-semibold text-3xl lg:text-4xl lg:leading-[4rem] mb-6 text-center lg:text-left text-primaryBlue">
+              Business Ready Solutions
+            </h1>
+            <div className="w-28 border border-primaryBlue mb-5"></div>
+
+            <div className="grid grid-cols-3 gap-20">
+              <div className="flex flex-col justify-center items-center text-center text-lg font-medium">
+                <BsBarChartLine className="text-4xl mb-7 text-primaryBlue" />
+                E-Commerce Solutions
+              </div>
+              <div className="flex flex-col justify-center items-center text-center text-lg font-medium">
+                <FaFolderOpen className="text-4xl mb-7 text-primaryBlue" />
+                ERP Solutions
+              </div>
+              <div className="flex flex-col justify-center items-center text-center text-lg font-medium">
+                <FaChalkboardUser className="text-4xl mb-7 text-primaryBlue" />
+                Management Software
+              </div>
+              <div className="flex flex-col justify-center items-center text-center text-lg font-medium">
+                <GoPerson className="text-4xl mb-7 text-primaryBlue" />
+                CRM Solutions
+              </div>
+              <div className="flex flex-col justify-center items-center text-center text-lg font-medium">
+                <GiBookmark className="text-4xl mb-7 text-primaryBlue" />
+                Content Management System
+              </div>
+              <div className="flex flex-col justify-center items-center text-center text-lg font-medium">
+                <FaPeopleCarry className="text-4xl mb-7 text-primaryBlue" />
+                Corporate Training Solutions
+              </div>
+            </div>
+          </div>
+          <img src={photo9} alt="" className="h-[20rem]" />
+        </div>
+      </div>
+      <div className="p-5 mb-40 flex items-start justify-center gap-40 bg-primaryBlue/10">
+        <div className="w-[75rem] flex items-center gap-10">
+          <img src={photo10} alt="" className="h-[20rem]" />
+          <div className="relative">
+            <h1 className="font-semibold text-3xl lg:text-4xl lg:leading-[4rem] mb-6 text-center lg:text-left text-primaryBlue">
+              Business Ready Solutions
+            </h1>
+            <div className="w-28 border border-primaryBlue mb-5"></div>
+
+            <div className="grid grid-cols-3 gap-20">
+              <div className="flex flex-col justify-center items-center text-center text-lg font-medium">
+                <LiaToolsSolid className="text-4xl mb-7 text-primaryBlue" />
+                Server Management, Monitoring and Security
+              </div>
+              <div className="flex flex-col justify-center items-center text-center text-lg font-medium">
+                <FaLock className="text-4xl mb-7 text-primaryBlue" />
+                Firewall Security Solution
+              </div>
+              <div className="flex flex-col justify-center items-center text-center text-lg font-medium">
+                <LiaKeySolid className="text-4xl mb-7 text-primaryBlue" />
+                Penetration Testing
+              </div>
+              <div className="flex flex-col justify-center items-center text-center text-lg font-medium">
+                <CiBasketball className="text-4xl mb-7 text-primaryBlue" />
+                Web Service Assessment
+              </div>
+              <div className="flex flex-col justify-center items-center text-center text-lg font-medium">
+                <PiFileMagnifyingGlassThin className="text-4xl mb-7 text-primaryBlue" />
+                Source Code Audit
+              </div>
+              <div className="flex flex-col justify-center items-center text-center text-lg font-medium">
+                <PiDeviceRotateThin className="text-4xl mb-7 text-primaryBlue" />
+                Security Architecture Review
+              </div>
+              <div className="flex flex-col justify-center items-center text-center text-lg font-medium">
+                <IoBugSharp className="text-4xl mb-7 text-primaryBlue" />
+                Vulnerability Assessment
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-5 py-20 mb-10 flex justify-center gap-40">
+        <div className="w-[70rem] flex items-start justify-center gap-20">
+          <div className="relative -top-25">
+            <h1 className="font-semibold text-3xl lg:text-4xl lg:leading-[4rem] mb-6 text-center lg:text-left text-primaryBlue">
+              IT Power Solutions
+            </h1>
+            <div className="w-28 border border-primaryBlue mb-2"></div>
+            <div className="text-gray-700 text-sm grid gap-5">
+              <p>
+                A reliable uninterrupted power supply is non-negotiable for IT
+                infrastructure. We understand that time is paramount, especially
+                when it comes to eliminating downtime and maximizing the life of
+                your backup power solution.
+              </p>
+              <p>
+                Our IT power solution shall be tailored to fit your company’s
+                unique requirements and infrastructure. We take proactive
+                approach from selection and implementation to maintenance and
+                upkeep
+              </p>
+            </div>
+          </div>
+          <img src={photo11} alt="" className="h-[15rem]" />
+        </div>
+      </div>
+      <div className="p-5 py-20 mb-10 flex justify-center gap-40 bg-primaryBlue/20">
+        <div className="w-[70rem] flex items-start justify-center gap-20">
+          <img src={photo12} alt="" className="h-[15rem]" />
+          <div className="relative -top-25">
+            <h1 className="font-semibold text-3xl lg:text-4xl lg:leading-[4rem] mb-6 text-center lg:text-left text-primaryBlue">
+              Corporate Training
+            </h1>
+            <div className="w-28 border border-primaryBlue mb-2"></div>
+            <div className="text-gray-700 text-sm">
+              <p>
+                Any solution provided becomes obsolete if it doesn’t impact your
+                primary objective – YOUR BUSINESS OUTCOMES. To mention a few,
+                whether it’s maximizing the productivity of your employees or
+                improving the quality and capabilities of company personnel, all
+                our interventions have one common DNA i.e. impacting business
+                outcome. Over the years, Center Point Multi Solutions Limited
+                has championed the art of impacting business of global clients
+                across multiple sectors through its customized IT security
+                training programs. Our corporate training programs provide
+                holistic solutions related to networking and security and we are
+                committed to a philosophy of excellence.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
 
