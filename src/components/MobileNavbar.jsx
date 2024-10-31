6c222421c6c9cb9788e627a0c923ab53ccfc2487
Dubby-Bantin <dubbybantin@gmail.com>
2024-10-31 10:27:44 +0100

    added links to navbar

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -1,60 +1,88 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import logo from "../assets/images/Asset 24.svg";
-import ExploreBtn from './buttons/ExploreBtn';
-import { FaBars } from 'react-icons/fa';
-import { LiaTimesSolid } from 'react-icons/lia';
-import { Link } from 'react-scroll';
+import ExploreBtn from "./buttons/ExploreBtn";
+import { FaBars } from "react-icons/fa";
+import { LiaTimesSolid } from "react-icons/lia";
+import { Link } from "react-scroll";
 
 const MobileNavbar = () => {
+  const [menuActive, setMenuActive] = useState(false);
+  const [isFirstRender, setIsFirstRender] = useState(true);
 
-    const [menuActive, setMenuActive] = useState(false);
-    const [isFirstRender, setIsFirstRender] = useState(true);
-
-    useEffect(() => {
-        if (menuActive) {
-            document.body.classList.add('no-scroll');
-        } else {
-            document.body.classList.remove('no-scroll');
-        }
-    }, [menuActive]);
-
-    const routes = [
-        {
-            label: "IT Solutions",
-            route: "solutions"
-        },
-        {
-            label: "Partners",
-            route: "partnership"
-        },
-        {
-            label: "Business Solutions",
-            route: "business-solutions"
-        },
-    ]
-
-    const handleMenuClick = () => {
-        setMenuActive(!menuActive);
-        setIsFirstRender(false);
+  useEffect(() => {
+    if (menuActive) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
     }
+  }, [menuActive]);
 
-    return (
-        <div className='lg:hidden block sticky top-0 z-50'>
-            <div className="sticky bg-[#0A0C2E] top-0 flex justify-between items-center z-50 px-6 py-4 border-b-[1px] border-[#0d204e]">
-                <img src={logo} alt="logo" className='w-[130px]'/>
+  const routes = [
+    {
+      label: "About",
+      route: "about",
+    },
+    {
+      label: "Contact",
+      route: "contact",
+    },
+    {
+      label: "Business Solutions",
+      route: "business-solutions",
+    },
+  ];
 
-                {menuActive ? <LiaTimesSolid onClick={handleMenuClick} className='text-white text-xl lg:hidden block'/> : <FaBars onClick={handleMenuClick}  className='text-white text-xl lg:hidden block'/>}
-            </div>
+  const handleMenuClick = () => {
+    setMenuActive(!menuActive);
+    setIsFirstRender(false);
+  };
 
-            <nav className={`${menuActive ? "flex animateNavbar" : isFirstRender ? "hidden" : "unAnimate"} mt-[2rem] lg:h-auto py-16 px-6 lg:px-none z-20 gap-14 flex lg:flex-row flex-col lg:w-auto w-full  lg:items-center lg:gap-6 text-sm lg:static absolute lg:bg-none bg-[#0A0C2E] left-0 lg:left-none top-0 lg:top-none`}>
-                    {routes.map(route => (
-                        <Link onClick={() => setMenuActive(false)} key={route.label} to={route.route} smooth="true" offset={10} duration={500}  className='text-gray-100 cursor-pointer' >{route.label}</Link>
-                    ))}
+  return (
+    <div className="lg:hidden block sticky top-0 z-50">
+      <div className="sticky bg-[#0A0C2E] top-0 flex justify-between items-center z-50 px-6 py-4 border-b-[1px] border-[#0d204e]">
+        <img src={logo} alt="logo" className="w-[130px]" />
 
-                    <ExploreBtn/>
-            </nav>
-        </div>
-    )
-}
+        {menuActive ? (
+          <LiaTimesSolid
+            onClick={handleMenuClick}
+            className="text-white text-xl lg:hidden block"
+          />
+        ) : (
+          <FaBars
+            onClick={handleMenuClick}
+            className="text-white text-xl lg:hidden block"
+          />
+        )}
+      </div>
 
-export default MobileNavbar
+      <nav
+        className={`${
+          menuActive
+            ? "flex animateNavbar"
+            : isFirstRender
+            ? "hidden"
+            : "unAnimate"
+        } mt-[2rem] lg:h-auto py-16 px-6 lg:px-none z-20 gap-14 flex lg:flex-row flex-col lg:w-auto w-full  lg:items-center lg:gap-6 text-sm lg:static absolute lg:bg-none bg-[#0A0C2E] left-0 lg:left-none top-0 lg:top-none`}
+      >
+        {routes.map((route) => (
+          <Link
+            onClick={() => setMenuActive(false)}
+            key={route.label}
+            // to={route.route}
+            href={route.route}
+            smooth="true"
+            offset={10}
+            duration={500}
+            className="text-gray-100 cursor-pointer"
+          >
+            {route.label}
+          </Link>
+        ))}
+
+        <ExploreBtn />
+      </nav>
+    </div>
+  );
+};
+
+export default MobileNavbar;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,57 @@
-import React from 'react'
+import React from "react";
 import logo from "../assets/images/Asset 24.svg";
-import ExploreBtn from './buttons/ExploreBtn';
-import MobileNavbar from './MobileNavbar';
-import { Link } from 'react-scroll';
+import ExploreBtn from "./buttons/ExploreBtn";
+import MobileNavbar from "./MobileNavbar";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
+  const routes = [
+    {
+      label: "About",
+      route: "about",
+    },
+    {
+      label: "Contact",
+      route: "contact",
+    },
+    {
+      label: "Business Solutions",
+      route: "business-solutions",
+    },
+    // {
+    //     label: "More",
+    //     route: "business-solutions",
+    //     subRoutes:[]
+    // },
+  ];
 
-    const routes = [
-        {
-            label: "IT Solutions",
-            route: "solutions"
-        },
-        {
-            label: "Partners",
-            route: "partnership"
-        },
-        {
-            label: "Business Solutions",
-            route: "business-solutions"
-        },
-        // {
-        //     label: "More",
-        //     route: "business-solutions",
-        //     subRoutes:[]
-        // },
-    ]
+  return (
+    <>
+      <header className="bg-[#0A0C2E] lg:flex hidden justify-between px-10 py-4 items-center top-0 sticky z-50">
+        <img src={logo} alt="logo" className="w-[175px] h-full" />
 
-    return (
-        <>
-            <header className='bg-[#0A0C2E] lg:flex hidden justify-between px-10 py-4 items-center top-0 sticky z-50'>
-                <img src={logo} alt="logo" className='w-[175px] h-full'/>
+        <nav className="flex items-center gap-6 text-sm">
+          {routes.map((route) => (
+            <Link
+              key={route.label}
+              to={route.route}
+              href={route.route}
+              smooth={true}
+              offset={10}
+              duration={500}
+              className="text-gray-100 cursor-pointer"
+            >
+              {route.label}
+            </Link>
+          ))}
 
-                <nav className='flex items-center gap-6 text-sm'>
-                    {routes.map(route => (
-                        <Link key={route.label} to={route.route} smooth={true}  offset={10}  duration={500}  className='text-gray-100 cursor-pointer' >{route.label}</Link>
-                    ))}
+          <ExploreBtn />
+        </nav>
+      </header>
 
-                    <ExploreBtn/>
-                </nav>
-            </header>
+      <MobileNavbar />
+    </>
+  );
+};
 
-            <MobileNavbar/>
-        </>
-    )
-}
-
-export default Navbar
+export default Navbar;
